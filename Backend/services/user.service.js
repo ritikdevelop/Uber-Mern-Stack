4828f3264c5935846668734114cd16911289da5f
ritikdevelop <hrithikrana216@gmail.com>
2025-11-26 00:41:11 +0530

    feat: implement createUser function in user service

---
+++ Backend/services/user.service.js
@@ -0,0 +1,21 @@
+const userModel = require("../models/user.model");
+
+module.exports.createUser = async ({
+  firstname,
+  lastname,
+  email,
+  password,
+}) => {
+  if (!firstname || !email || !password) {
+    throw new Error("All required fields must be filled");
+  }
+  const user = userModel({
+    fullname: {
+      firstname,
+      lastname,
+    },
+    email,
+    password,
+  });
+  return await user.save();
+};
