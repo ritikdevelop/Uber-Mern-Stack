6ca38fe9c9ad73dd7fb6177783e926f2bae3df75
ritikdevelop <hrithikrana216@gmail.com>
2025-11-26 12:36:49 +0530

    Handles the logic for captain registration, login, and profile retrieval, calling the corresponding service functions.

---
+++ Backend/controllers/captain.controller.js
@@ -0,0 +1,74 @@
+const captainModel = require("../models/captain.model");
+const captainService = require("../services/captain.service");
+const { validationResult } = require("express-validator");
+const blackListTokenModel = require("../models/blacklistToken.model");
+
+module.exports.registerCaptain = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { fullname, email, password, vehicle } = req.body;
+  //   console.log(req.body);
+
+  const isCaptainAlreadyExist = await captainModel.findOne({
+    email,
+  });
+  if (isCaptainAlreadyExist) {
+    return res.status(409).json({ message: "Captain already exists" });
+  }
+
+  const hashedPassword = await captainModel.hashPassword(password);
+
+  const captain = await captainService.createCaptain({
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashedPassword,
+    color: vehicle.color,
+    plate: vehicle.plate,
+    capacity: vehicle.capacity,
+    vehicleType: vehicle.vehicleType,
+  });
+  const token = captain.generateAuthToken();
+  res.status(201).json({ captain, token });
+};
+
+module.exports.loginCaptain = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+  console.log(req.body);
+
+  const captain = await captainModel.findOne({ email }).select("+password");
+
+  if (!captain) {
+    return res.status(401).json({ message: "Captain not found" });
+  }
+  const isMatch = await captain.comparePassword(password);
+
+  if (!isMatch) {
+    return res.status(401).json({ message: "Password does not match" });
+  }
+
+  const token = captain.generateAuthToken();
+  res.cookie("token", token);
+  res.status(200).json({ captain, token });
+};
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({ captain: req.captain });
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+
+    await blackListTokenModel.create({ token });
+
+    res.clearCookie('token');
+
+    res.status(200).json({ message: 'Logout successfully' });
+}
