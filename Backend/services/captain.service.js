30898cac2fa72d7ef1477d6acb6076aed463a868
ritikdevelop <hrithikrana216@gmail.com>
2025-11-26 12:36:36 +0530

    Contains functions for creating a new captain, logging in a captain, and retrieving a captain's profile.

---
+++ Backend/services/captain.service.js
@@ -0,0 +1,42 @@
+const { verify } = require("jsonwebtoken");
+const captainModel = require("../models/captain.model");
+
+module.exports.createCaptain = async ({
+  firstname,
+  lastname,
+  email,
+  password,
+  color,
+  plate,
+  capacity,
+  vehicleType,
+}) => {
+  if (
+    !firstname ||
+    !email ||
+    !password ||
+    !color ||
+    !plate ||
+    !capacity ||
+    !vehicleType
+  ) {
+    throw new Error("All required fields must be filled");
+  }
+
+  const captain = captainModel.create({
+    fullname: {
+      firstname,
+      lastname,
+    },
+    email,
+    password,
+    vehicle: {
+      color,
+      plate,
+      capacity,
+      vehicleType,
+    },
+  });
+
+  return captain;
+};
