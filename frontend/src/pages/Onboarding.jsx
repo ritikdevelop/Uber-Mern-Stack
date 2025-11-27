ce91718ad261d04cc1abf46f4ad9a499f8c7c513
ritikdevelop <hrithikrana216@gmail.com>
2025-11-27 14:32:57 +0530

    Created a onboarding page

---
+++ frontend/src/pages/Onboarding.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ArrowRight } from "lucide-react";
+
+const Start = () => {
+  return (
+    <div>
+      <div className="bg-cover bg-center bg-[url(https://uberhrb.netlify.app/assets/homeBg-BTLwEeDj.jpg)] h-screen pt-8 flex justify-between flex-col w-full">
+        <img
+          className="w-16 ml-8"
+          src="https://uberhrb.netlify.app/assets/Uber-Logo-C3bS_wjK.png"
+          alt="Cover Image"
+        />
+        <div className="bg-white pb-8 py-4 px-4">
+          <h2 className="text-[30px] font-semibold">Get Started with Uber</h2>
+          <Link
+            to="/login"
+            className="flex items-center justify-center w-full bg-black text-white py-3 rounded-lg mt-5"
+          >
+            <h2 className="flex justify-center items-center font-bold text-xl">
+              Continue
+              {/* <ArrowRight size={18} strokeWidth={3} className="ml-auto" /> */}
+            </h2>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
