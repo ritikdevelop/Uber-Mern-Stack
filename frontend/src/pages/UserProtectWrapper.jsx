2a36126e812985ce21f995b428dacd2d0a935a6b
ritikdevelop <hrithikrana216@gmail.com>
2025-12-07 19:30:01 +0530

    `Added user and captain authentication routes and components`

---
+++ frontend/src/pages/UserProtectWrapper.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../context/UserContext";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+const UserProtectWrapper = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserDataContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setUser(response.data);
+          setIsLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        localStorage.removeItem("token");
+        navigate("/login");
+      });
+  }, [token]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return <>{children}</>;
+};
+
+export default UserProtectWrapper;
